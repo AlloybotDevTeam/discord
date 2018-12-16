a20d4d29086e0aad39221658d0527c0c6323ca8d
Taylor Stapleton <stapletontaylor@hotmail.com>
2018-12-15 17:31:57 -0700

    Workin

--- Discord.ts
+++ Discord.ts
@@ -1,38 +1,15 @@
-import { IConnection, Alloybot, DependantList } from '../../../Alloybot';
-import { Shard, ShardingManager } from 'discord.js';
+import { default as Alloybot, Type, Util } from 'Alloybot';
+import { Client } from 'discord.js';
 
-export default class Discord implements IConnection {
+export default class Discord implements Type.IConnection {
   public readonly name: string = 'Discord';
   public readonly dependencies: string[] = [];
-  public readonly dependants: DependantList = Alloybot.getDependants(this.name);
-  public connection: Map<string, ShardingManager> = new Map();
+  public readonly dependants: Type.IPlugin[] = Alloybot.getDependants(this.name);
+  public readonly connection: Client = new Client();
 
-  private shard_limit: number;
-
-  constructor(options: string) {
-    this.shard_limit = JSON.parse(options).SHARD_LIMIT;
-  }
-
-  public createShard(
-    name: string,
-    file: string,
-    options: {
-      totalShards?: number | 'auto';
-      respawn?: boolean;
-      shardArgs?: string[];
-      token?: string;
-    }
-  ): ShardingManager {
-    let shardman: ShardingManager;
-    if (<number>options.totalShards > this.shard_limit) {
-      options.totalShards = this.shard_limit;
-      shardman = new ShardingManager(file, options);
-    } else {
-      shardman = new ShardingManager(file, options);
-    }
-    this.connection.set(name, shardman);
-    return shardman;
+  constructor(token: string) {
+    this.connection.login(token);
   }
 }
 
-Alloybot.registerConnection(new Discord(process.env['DISCORD']));
+Alloybot.registerConnection(new Discord(process.env["DISCORD_TOKEN"]));
