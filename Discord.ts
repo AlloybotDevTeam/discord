0dda0e024db048f4a828bd30b37c6f16058e3a50
Taylor Stapleton <stapletontaylor@hotmail.com>
2018-12-16 22:32:57 -0700

    LETS GET IT

--- Discord.ts
+++ Discord.ts
@@ -1,4 +1,4 @@
-import { default as Alloybot, Type, Util } from 'Alloybot';
+import { default as Alloybot, Type, Util, ConfigBuilder } from '../../Alloybot';
 import { Client } from 'discord.js';
 
 export default class Discord implements Type.IConnection {
@@ -6,10 +6,19 @@ export default class Discord implements Type.IConnection {
   public readonly dependencies: string[] = [];
   public readonly dependants: Type.IPlugin[] = Alloybot.getDependants(this.name);
   public readonly connection: Client = new Client();
+  public config;
 
-  constructor(token: string) {
-    this.connection.login(token);
+  constructor() {
+    let Config: ConfigBuilder = new ConfigBuilder('Discord', require('./package.json').version);
+    Config.addOption('token', ['string'], 'Discord Bot Token');
+    Config.close();
+    this.config = Config.getConfig();
+  }
+
+  public connect(): Discord {
+    this.connection.login(this.config.token);
+    return this;
   }
 }
 
-Alloybot.registerConnection(new Discord(process.env["DISCORD_TOKEN"]));
+Alloybot.registerConnection(new Discord().connect());
